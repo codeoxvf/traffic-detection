5ea17f52a9744ccde1f0fa7cb9559f024e6ae3fa
codeo <13805417+codeoxvf@users.noreply.github.com>
2022-07-20 16:13:44 +0800

    Cleaned up prototype display code

--- main.py
+++ main.py
@@ -1,55 +1,39 @@
 import tensorflow as tf
-from numpy import expand_dims, argmax
+from numpy import stack
 from matplotlib import pyplot as plt
 from matplotlib import image as img
 
-image1 = 'data/night/1004.jpg'
-image2 = 'data/night/1704.jpg'
-
 # load images
-q = tf.keras.preprocessing.image.img_to_array(
-    tf.keras.preprocessing.image.load_img(
-        image1, target_size=(256, 256)
-    )
-)
-f = tf.keras.preprocessing.image.img_to_array(
-    tf.keras.preprocessing.image.load_img(
-        image2, target_size=(256, 256)
-    )
-)
-q = expand_dims(q, axis=0)
-f = expand_dims(f, axis=0)
+image_names = ['data/day/4714.jpg', 'data/day/5799.jpg', 'data/day/2705.jpg',
+               'data/day/1703.jpg']
+
+# format images as tensor
+arr = [tf.keras.utils.img_to_array(
+    tf.keras.utils.load_img(i, target_size=(256, 256)))
+    for i in image_names]
+
+data = stack(arr)
 
 # load saved model
 model = tf.keras.models.load_model('model')
 
-#labels = ['b', 'f', 'i', 'q']
-#print('Categorical labels:', labels)
-
 # run model
-pred1 = model.predict_on_batch(q)
-pred2 = model.predict_on_batch(f)
-print('Image 1', pred1)
-print('Free-flow' if pred1[0][0] < 0 else 'Queue state')
-#print('Label:', labels[int(argmax(pred1, axis=1)[0])])
-print('Image 2', pred2)
-print('Free-flow' if pred2[0][0] < 0 else 'Queue state')
-#print('Label:', labels[int(argmax(pred2, axis=1)[0])])
+preds = model.predict_on_batch(data)
 
 # display images
-img1 = img.imread(image1)
-img2 = img.imread(image2)
+images = [img.imread(i) for i in image_names]
 
 fig = plt.figure()
 
-sub1 = fig.add_subplot(2, 1, 1)
-sub1.set_title('Image 1')
-sub1.axis('off')
-sub1.imshow(img1)
+for i in range(len(image_names)):
+    sub = fig.add_subplot(len(image_names)//2, 2, i+1)
+    sub.set_title('Image ' + str(i) + ': ' + image_names[i].split('/')[-1])
+    sub.set_xlabel('Prediction: '
+                   + ('Free-flow' if preds[i][0] < 0 else 'Queue state')
+                   + ' ' + str(preds[i]))
 
-sub2 = fig.add_subplot(2, 1, 2)
-sub2.imshow(img2)
-sub2.set_title('Image 2')
-sub2.axis('off')
+    sub.set_xticks([])
+    sub.set_yticks([])
+    sub.imshow(images[i], interpolation='none')
 
 plt.show()
